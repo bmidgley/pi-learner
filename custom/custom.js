864991f731bf65797e97cd92edec123e36e7e6ba
Brad Midgley <bmidgley@gmail.com>
2017-02-02 22:53:34 -0700

    noconsolelog

--- custom/custom.js
+++ custom/custom.js
@@ -1,6 +1,5 @@
 define(['base/js/events'], function(events) {
   events.on('app_initialized.NotebookApp', function () {
-    console.log('custom.js: hiding header and toolbar');
     $('#header-container').toggle();
     $('.header-bar').toggle();
     $('div#maintoolbar').toggle();
